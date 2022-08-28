d300547916677937a2f293294f7fa03b28938a5b
Mulloa90 <mario90gm@icloud.com>
2022-08-28 14:17:58 -0700

    updated add notes route

--- db/store.js
+++ db/store.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const util = require("util");
-const { uuidv4 } = require("uuid");
+const { v4 } = require("uuid");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -9,12 +9,26 @@ class Store {
   read() {
     return readFileAsync("db/db.json", "utf8");
   }
-  write() {}
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
 
   getNotes() {
     return this.read().then((notes) => {
-      return [].concat(JSON.parse(notes))
+      return [].concat(JSON.parse(notes));
     });
+  }
+  addNote(note) {
+    note.id = v4();
+
+    if (!note.title || !note.text) {
+      throw new Error("Title or Text cannot be blank");
+    }
+
+    return this.getNotes()
+      .then((notes) => [...notes, note])
+      .then((result) => this.write(result))
+      .then(() => note);
   }
 }
 
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -8,4 +8,9 @@ router.get("/notes", (req, res) =>
     .catch((err) => res.status(500).json(err))
 );
 
+router.post('/notes', (req,res)=>
+    store.addNote(req.body).then((note)=>res.json(note))
+    .catch((err) => res.status(500).json(err))
+);
+
 module.exports = router;
