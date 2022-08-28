1cc6626fc354046ea4764f665339744edbcff699
Mulloa90 <mario90gm@icloud.com>
2022-08-28 13:08:58 -0700

    updated Html routes

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+
+const html = require('./routes/htmlroutes')
+// const api = require('./routes/apiroutes')
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+// app.use('/api', api);
+ app.use('/', html);
+
+
+
+
+// GET Route for homepage
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
+
+// // GET Route for feedback page
+// app.get('/feedback', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+// );
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
