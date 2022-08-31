cee727cb293f8853d4f628b0a9b03bcd220a2c4b
Mulloa90 <mario90gm@icloud.com>
2022-08-30 21:38:39 -0700

    updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const html = require('./routes/htmlroutes')
 const api = require('./routes/apiroutes')
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORTgit  || 3001;
 
 const app = express();
 
